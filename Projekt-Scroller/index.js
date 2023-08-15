91a064221b41d6efb682ae915489b5818ffd630a
Vneder <4007mj@gmail.com>
2023-08-15 14:53:25 +0200

    1/2 scrollera

---
+++ Projekt-Scroller/index.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const rootElement = document.querySelector('.root');
+    const sections = document.querySelectorAll('section');
+    let currentSectionIndex = 0;
+    let isThrottled = false;
+
+    document.addEventListener('wheel', function (e) {
+        if (isThrottled) return;
+        isThrottled = true;
+
+        setTimeout(function () {
+            isThrottled = false;
+        }, 1000);
+
+        const direction = e.deltaY < 0 ? 1 : -1;
+
+        scroll(direction);
+    });
+
+    function scroll(direction) {
+        if (direction === 1) {
+            const isLastSection = currentSectionIndex === sections.length - 1;
+            if (isLastSection) {
+                return;
+            }
+        }
+        else if (direction === -1) {
+            const isFirstSection = currentSectionIndex === 0;
+            if (isFirstSection) {
+                return;
+            }
+        }
+
+        currentSectionIndex = currentSectionIndex + direction;
+
+        scrollToCurrentSection();
+    }
+
+    function scrollToCurrentSection() {
+        sections[currentSectionIndex].scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    }
+
+}) 
